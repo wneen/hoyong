8f772fa7988eb5708d8d0aa8d91de3137a19508c
wneen <wneen@naver.com>
2014-10-03 16:14:47 +0900

    rm .npm

--- .npm/mows/0.0.6/package/buildWebsocketBrowser.js
+++
@@ -1,8 +0,0 @@
-
-var websocket = require('websocket-stream')
-
-module.exports = function buildWebsocket(url, opts) {
-  opts.protocol = 'mqttv3.1';
-  opts.type = Uint8Array;
-  return websocket(url, opts);
-};
